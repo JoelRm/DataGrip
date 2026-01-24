d766119f7930c32bdeec0b67e2ee2daa8810aee0
Joel <joelrojas43@gmail.com>
2026-01-24 14:34:37 -0500

    Poject Cerdyn

--- SQL/PruebaSQL.sql
+++
@@ -1,5 +0,0 @@
-select 'PVOL-' || rv.RANGO1  || '@' || round((rv.PRECIO - rv.PRECIO1) * 100 / rv.PRECIO, 2) nombre_promo, rv.DESCRIPCION, rv.INICIO, rv.FIN, rv.SKU, rv.RANGO1, --(rv.PRECIO - rv.PRECIO1) * 100 / rv.PRECIO,
-       round((rv.PRECIO - rv.PRECIO1) * 100 / rv.PRECIO, 2) porcentaje_dscto
-    --,rv.PRECIO, rv.PRECIO1, p.DES_EST
-from EDSR.HPRANGOPRECIO rv inner join EDSR.tpprdmst p on rv.PRD_LVL_CHILD = p.PRD_LVL_CHILD
-where rv.SUCURSAL = 517 and p.DES_EST <> 'Excluido';
